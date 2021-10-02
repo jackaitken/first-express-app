d198e6dff18bcecb63bba35eab1165ac66d39e92
Jack Aitken <jaitk11@gmail.com>
2021-10-02 19:52:22 -0400

    Initial commit

---
+++ hello.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  res.render('hello-world-english');
+});
+
+app.listen(3000, 'localhost', () => {
+  console.log('Listening on port 3000.');
+});

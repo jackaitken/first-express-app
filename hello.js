562e7afff249cfd052c51f87ff834739ff7fab6e
Jack Aitken <jaitk11@gmail.com>
2021-10-03 15:41:44 -0400

    Added layout page

--- hello.js
+++ hello.js
@@ -1,17 +1,23 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 app.set('views', './views');
 app.set('view engine', 'pug');
 
 app.use(express.static('public'));
+app.use(morgan('common'));
 
 app.get('/', (req, res) => {
-  res.render('hello-world-english');
+  res.redirect('/english');
 });
 
 app.get('/english', (req, res) => {
   res.render('hello-world-english');
+});
+
+app.get('/spanish', (req, res) => {
+  res.render('hello-world-spanish');
 });
 
 app.get('/french', (req, res) => {

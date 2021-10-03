2dd915223a81f05035b854c637b4b1ffe939a514
Jack Aitken <jaitk11@gmail.com>
2021-10-02 23:57:31 -0400

    Added French and Serbian

--- hello.js
+++ hello.js
@@ -4,8 +4,22 @@ const app = express();
 app.set('views', './views');
 app.set('view engine', 'pug');
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.render('hello-world-english');
+});
+
+app.get('/english', (req, res) => {
+  res.render('hello-world-english');
+});
+
+app.get('/french', (req, res) => {
+  res.render('hello-world-french');
+});
+
+app.get('/serbian', (req, res) => {
+  res.render('hello-world-serbian');
 });
 
 app.listen(3000, 'localhost', () => {
